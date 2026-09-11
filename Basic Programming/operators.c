5df5267f58bb660e051c49a28db30d7d521f50f0
pedripottah <pedripottah@gmail.com>
2026-09-11 23:33:11 +0700

    Artihmetic operators

---
+++ Basic Programming/operators.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    // arithmetic operators ( = + - / % ++ --)
+
+    int x = 10;
+    float y = 3;
+    float z = 1;
+
+    // z = x + y;
+    // z = x - y;
+    // z = x * y;
+    // z = x / y;
+    // z = x % 3;
+
+    // x++;
+    // x--;
+    // x += 2;
+    // x -= 2;
+
+    printf("%d\n", x);
+    printf("%.2f", z);
+}
